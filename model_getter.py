ba621890feae39226ccf3d8075651fb3fe4bdb27
alonzi <alonzi@cs.huji.ac.il>
2023-07-15 23:26:22 +0300

    LinearAcousticCTC - v1

--- model_getter.py
+++ model_getter.py
@@ -1,4 +1,7 @@
 from models.linear_acoustic_model import LinearAcoustic
 
 def get_model(args, cfg):
-    return LinearAcoustic(cfg)
+    if cfg.model == "LinearAcoustic":
+        return LinearAcoustic(cfg)
+    else:
+        raise NotImplementedError
